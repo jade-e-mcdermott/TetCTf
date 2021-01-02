20f0dfe6fe53c01eec629250b579eadd99ba88d0
jade-e-mcdermott <jade.e.mcdermott@gmail.com>
2021-01-01 21:39:46 -0800

    Started work on SuperCalc

---
+++ SuperCalc/main.py
@@ -0,0 +1,40 @@
+import requests
+
+
+# Helper Functions
+def InjectedStuffIsTooLong(stringToCheck):
+    if (len(stringToCheck) > 70):
+        return True
+    else:
+        return False
+
+def TryInjectedPayload(injectedPayload):
+    baseUrl = "http://139.180.155.171?calc="
+    fullRequest = baseUrl + injectedPayload
+    if(InjectedStuffIsTooLong(injectedPayload) == False):
+        response = requests.get(fullRequest)
+        print("Stats: ", response.status_code)
+        return response.text
+    else:
+        return "Injected payload too long."
+
+def TryInjectedPayloadOnLocal(injectedPayload):
+    baseUrl = "http://localhost:8081?calc="
+    fullRequest = baseUrl + injectedPayload
+    if(InjectedStuffIsTooLong(injectedPayload) == False):
+        response = requests.get(fullRequest)
+        print("Stats: ", response.status_code)
+        return response.text
+    else:
+        return "Injected payload too long."
+
+# Working Hax
+
+
+
+
+# Main
+injectedPayload = "146&02"
+results = TryInjectedPayloadOnLocal(injectedPayload)
+print(results)
+
---
+++ SuperCalc/source.php
@@ -0,0 +1,22 @@
+<!-- Enjoy Tsu's Super Calculator <3, Not Only + - * / but also many other operators <3 <3 <3 -->
+
+<?php
+
+ini_set("display_errors", 0);
+
+if(!isset($_GET["calc"])) 
+{
+    show_source(__FILE__);
+}
+else
+{
+    $wl = preg_match('/^[0-9\+\-\*\/\(\)\'\.\~\^\|\&]+$/i', $_GET["calc"]);
+    if($wl === 0 || strlen($_GET["calc"]) > 70) {
+        die("Tired of calculating? Lets <a href='https://www.youtube.com/watch?v=wDe_aCyf4aE' target=_blank >relax</a> <3");
+    }
+    echo 'Result: ';
+    echo $_GET["calc"];
+    eval("echo ".eval("return ".$_GET["calc"].";").";");
+}
+//echo result of:
+//return 2*2;
